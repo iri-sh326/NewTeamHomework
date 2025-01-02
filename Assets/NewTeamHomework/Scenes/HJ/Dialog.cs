604467de32e28802f5e6652773588012e7dc55fb
nsztte <rlalwjd41@gmail.com>
2025-01-02 17:53:14 +0900

    (01.02) 다이얼로그 추가

---
+++ Assets/NewTeamHomework/Scenes/HJ/Dialog.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace NewTeamHomework
+{
+    public class Dialog
+    {
+        public int number;
+        public int character;
+        public string name;
+        public string sentence;
+    }
+}
