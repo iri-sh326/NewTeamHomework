7d48720432ce8b47899db55c967a88ef93ea3ee8
iri-sh326 <bf980326@gmail.com>
2025-01-03 17:11:34 +0900

    엔딩씬

--- Assets/NewTeamHomework/Scenes/Sohyeon/Socket.cs
+++ Assets/NewTeamHomework/Scenes/Sohyeon/Socket.cs
@@ -1,7 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class Socket : MonoBehaviour
 {
+    public Transform _transform;
+    public Canvas canvas;
+    public TextMeshProUGUI text;
     public GameObject ball_1;
     public GameObject ball_2;
     public GameObject ball_3;
@@ -15,6 +19,8 @@ public class Socket : MonoBehaviour
         if(isCorrect_1 && isCorrect_2 && isCorrect_3)
         {
             Debug.Log("정답");
+            canvas.transform.position = (_transform.position);
+            text.text = "정답입니다~!";
         }
         else
         {
